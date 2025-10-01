5e18d02021f2f1056887aaedfedd110827546f43
iidexic <barrierloss@yahoo.com>
2025-09-30 19:44:28 -0700

    WIP refactor: extend + reorganize assembly of colors table

--- lua/material/colors/apply_groups.lua
+++ lua/material/colors/apply_groups.lua
@@ -5,9 +5,9 @@ M.existing_groups = {
 	"alternate",
 	"poopy",
 }
-
 local groups = {}
 
+--- Requires colors table to generate style groups
 function M.make_groups(colors)
 	groups.default = {
 		syntax = {
@@ -36,6 +36,37 @@ function M.make_groups(colors)
 			floating_windows = colors.editor.bg,
 			non_current_windows = colors.editor.bg,
 			bg_blend = colors.editor.bg, -- backup used for blending backgrounds (issue: #212)
+			cursor_line = colors.editor.active,
+		},
+	}
+	groups.defaultSidebar = {
+		editor = {
+			variable = colors.editor.fg,
+			field = colors.editor.green,
+			keyword = colors.main.purple,
+			value = colors.main.orange,
+			operator = colors.main.cyan,
+			fn = colors.main.blue,
+			parameter = colors.main.paleblue,
+			string = colors.main.green,
+		},
+		syntax = {
+			type = colors.main.purple,
+		},
+		git = {
+			added = colors.main.green,
+			removed = colors.main.red,
+			modified = colors.main.blue,
+		},
+		lsp = {
+			warning = colors.main.yellow,
+			info = colors.main.paleblue,
+			hint = colors.main.purple,
+		},
+		backgrounds = { -- function to check if sidebar currently bg?
+			floating_windows = colors.editor.bg,
+			--sidebars = colors.editor.bg,
+			non_current_windows = colors.editor.bg,
 			cursor_line = colors.editor.active,
 		},
 	}
@@ -99,11 +130,72 @@ function M.make_groups(colors)
 			cursor_line = colors.editor.active,
 		},
 	}
+	groups.monokai = {
+		syntax = {
+			variable = colors.editor.fg,
+			field = colors.editor.purple,
+			keyword = colors.main.red,
+			value = colors.main.purple,
+			operator = colors.main.red,
+			fn = colors.main.green,
+			parameter = colors.main.darkyellow,
+			string = colors.main.yellow,
+			type = colors.main.blue,
+		},
+		git = {
+			added = colors.main.green,
+			removed = colors.main.red,
+			modified = colors.main.blue,
+		},
+		lsp = {
+			warning = colors.main.yellow,
+			info = colors.main.paleblue,
+			hint = colors.main.purple,
+		},
+		backgrounds = {
+			sidebars = colors.editor.bg,
+			floating_windows = colors.editor.bg,
+			non_current_windows = colors.editor.bg,
+			bg_blend = colors.editor.bg,
+			cursor_line = colors.editor.active,
+		},
+	}
+	groups.warm = {
+		syntax = {
+			variable = colors.editor.fg,
+			field = colors.editor.yellow,
+			keyword = colors.main.red,
+			value = colors.main.blue,
+			operator = colors.main.red,
+			fn = colors.main.green,
+			parameter = colors.main.orange,
+			string = colors.main.cyan,
+			type = colors.main.darkred,
+		},
+		git = {
+			added = colors.main.green,
+			removed = colors.main.red,
+			modified = colors.main.blue,
+		},
+		lsp = {
+			warning = colors.main.yellow,
+			info = colors.main.paleblue,
+			hint = colors.main.blue,
+		},
+		backgrounds = {
+			sidebars = colors.editor.black,
+			floating_windows = colors.editor.bg,
+			non_current_windows = colors.editor.bg,
+			bg_blend = colors.editor.bg,
+			cursor_line = colors.editor.active,
+		},
+	}
+	M.groups = groups
 end
 ---sets how colors are applied to vim highlight groups
 ---@param colors table colors table to apply to
 ---@param id string name of apply group
-function M.apply_group(colors, id)
+function M.apply(colors, id)
 	if groups[id] then
 		return vim.tbl_deep_extend("force", colors, groups[id])
 	end
